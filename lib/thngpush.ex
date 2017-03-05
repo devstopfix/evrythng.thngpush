ed49057e99a575d6b729552d7fd2a302a71320a3
devstopfix <devstopfix@gmail.com>
2017-03-05 21:20:54 +0000

    chore: readme and empty project

---
+++ lib/thngpush.ex
@@ -0,0 +1,18 @@
+defmodule Evrythng.ThngPush do
+  @moduledoc """
+  Documentation for Evrythng.ThngPush.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Evrythng.ThngPush.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/thngpush_test.exs
@@ -0,0 +1,8 @@
+defmodule Evrythng.ThngPushTest do
+  use ExUnit.Case
+  doctest Evrythng.ThngPush
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
