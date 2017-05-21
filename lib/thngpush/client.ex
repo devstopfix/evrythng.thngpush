12ab3155cd4f672334608daf56839bf51438ab86
devstopfix <james@devstopfix.net>
2017-05-21 18:00:47 +0100

    UserAgent contains library version number

--- lib/thngpush/client.ex
+++ lib/thngpush/client.ex
@@ -1,5 +1,5 @@
 defmodule Evrythng.ThngPush.Client do
-	
+
   use GenServer
 
   def start_link(api_key) do
@@ -27,18 +27,25 @@ defmodule Evrythng.ThngPush.Client do
   def handle_info({:mqttc, _pid, :connected}) do
     IO.puts("Connected");
   end
-	
+
   @doc """
   Variation of the EMQTTC client id using the host name, user agent and random number.
 
   See [https://github.com/emqtt/emqttc/blob/815ebeca103025bbb5eb8e4b2f6a5f79e1236d4c/src/emqttc_protocol.erl#L125](emqttc_protocol.erl#L125)
   """
   def generate_client_id do
-     {:ok, host} = :inet.gethostname()
-     :rand.seed(:exsplus)
-     i1 = :rand.uniform(round(:math.pow(2, 48))) - 1
-     i2 = :rand.uniform(round(:math.pow(2, 32))) - 1
-     ["Elixir/ThngPush", host, (:io_lib.format("~12.16.0b~8.16.0b", [i1, i2]) |> to_string)] |> Enum.join("_")
+    {:ok, host} = :inet.gethostname()
+    :rand.seed(:exsplus)
+    i1 = :rand.uniform(round(:math.pow(2, 48))) - 1
+    i2 = :rand.uniform(round(:math.pow(2, 32))) - 1
+    [user_agent(), host, (:io_lib.format("~12.16.0b~8.16.0b", [i1, i2]) |> to_string)] |> Enum.join("_")
   end
+
+  # UserAgent
+
+  @version Mix.Project.config[:version]
+  def version(), do: @version
+
+  def user_agent(), do: "Elixir/ThngPush/" <> version()
 
 end
--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Evrythng.ThngPush.Mixfile do
 
   def project do
     [app: :thngpush,
-     version: "0.17.60",
+     version: "0.17.137",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
